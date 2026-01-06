3cfe484c84849213f0b1f419d81603633a7007db
Jacob O'Donnell <jacobodonnell96@proton.me>
2026-01-06 16:14:15 -0600

    feat: allow product table filtering

--- app/Filament/Resources/Products/Tables/ProductsTable.php
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -2,12 +2,18 @@
 
 namespace App\Filament\Resources\Products\Tables;
 
+use App\Enums\ProductStatusEnum;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Forms\Components\DatePicker;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
 class ProductsTable
 {
@@ -27,8 +33,28 @@ class ProductsTable
             ])
             ->defaultSort('price', 'desc')
             ->filters([
-                //
-            ])
+                SelectFilter::make('status')
+                            ->options(ProductStatusEnum::class),
+                SelectFilter::make('category')
+                            ->relationship('category', 'name')
+                            ->multiple(),
+                Filter::make('created_at')
+                      ->schema([
+                          DatePicker::make('created_from'),
+                          DatePicker::make('created_until'),
+                      ])
+                      ->query(function (Builder $query, array $data): Builder {
+                          return $query
+                              ->when(
+                                  $data['created_from'],
+                                  fn(Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                              )
+                              ->when(
+                                  $data['created_until'],
+                                  fn(Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                              );
+                      })
+            ], layout: FiltersLayout::Modal)
             ->recordActions([
                 EditAction::make(),
                 DeleteAction::make()
